7f533682d2e171f5ecffe8f7559b5020e0d3aedf
qTxTp <19466769+qTxTp@users.noreply.github.com>
2021-01-05 19:51:55 +0800

    NONE

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,12 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.25.54/g' package/base-files/files/bin/config_generate
+
+# Upgrade qBittorrent to the latest available version
+sed -i '4c PKG_VERSION:=4.3.2' package/lean/qBittorrent/Makefile
+sed -i '9c PKG_HASH:=37d49b70ba5319c3dc208f22a503671e21b1212b8099b8bfff1ca570fed32360' package/lean/qBittorrent/Makefile
+
+# Upgrade rblibtorrent to the latest available version
+sed -i '4c PKG_VERSION:=1.2.12' package/lean/rblibtorrent/Makefile
+sed -i '10c PKG_SOURCE_VERSION:=1bc1df76e46fcd2cdb89c5cc7795d1e126ff4106' package/lean/rblibtorrent/Makefile
--- diy-part3.sh
+++ diy-part3.sh
@@ -11,9 +11,8 @@
 #
 
 # Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git diy1 https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git openclash https://github.com/vernesong/OpenClash' feeds.conf.default
