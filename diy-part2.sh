86ae466b440ff9f80927386b953e04b61e06f68c
qTxTp <19466769+qTxTp@users.noreply.github.com>
2021-01-21 20:13:23 +0800

    Update qBittorrent

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,8 +14,8 @@
 sed -i 's/192.168.1.1/192.168.25.54/g' package/base-files/files/bin/config_generate
 
 # Upgrade qBittorrent to the latest available version
-sed -i '4c PKG_VERSION:=4.3.2' package/lean/qBittorrent/Makefile
-sed -i '9c PKG_HASH:=37d49b70ba5319c3dc208f22a503671e21b1212b8099b8bfff1ca570fed32360' package/lean/qBittorrent/Makefile
+sed -i '4c PKG_VERSION:=4.3.3' package/lean/qBittorrent/Makefile
+sed -i '9c PKG_HASH:=b3f91bf3852aa166308ae133318e60f9b988663396078f59e78e90ce91541723' package/lean/qBittorrent/Makefile
 
 # Upgrade rblibtorrent to the latest available version
 sed -i '4c PKG_VERSION:=1.2.12' package/lean/rblibtorrent/Makefile
